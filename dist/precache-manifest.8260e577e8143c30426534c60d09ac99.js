60e5be16e0d2e8bf00a215e0ad5f53ca88094ae6
Dominik Tarnowski <dominik.t.uk@gmail.com>
2018-08-27 02:06:10 +0100

    add /dist

---
+++ dist/precache-manifest.8260e577e8143c30426534c60d09ac99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d41361b4cf52516481d9",
+    "url": "/js/chunk-vendors.9c24ef5c.js"
+  },
+  {
+    "revision": "71de01704b4cfc849cb9",
+    "url": "/js/app.098b29d3.js"
+  },
+  {
+    "revision": "0cf5b2f1aff9fbfe02b2",
+    "url": "/js/about.f2dc9309.js"
+  },
+  {
+    "revision": "2027206db0d86cdb2664c0298511da17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d41361b4cf52516481d9",
+    "url": "/css/chunk-vendors.0df7dd5e.css"
+  },
+  {
+    "revision": "71de01704b4cfc849cb9",
+    "url": "/css/app.3060ed41.css"
+  }
+];
